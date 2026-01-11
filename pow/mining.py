55518090b653be1c1417072cab90d4ac26c60107
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-01-10 21:04:38 -0600

    Update mining.py

--- pow/mining.py
+++ pow/mining.py
@@ -3,13 +3,14 @@ from hashlib import sha256
 from pathlib import Path
 from pow import block
 
+block.propagate()
+
 with open(f"{Path(__file__).parent.parent}/config.json", "r") as r:
   config = json.load(r)
 with open(f"{Path(__file__).parent.parent}/data/wordChain.json", "r") as r:
   wordChain = json.load(r)
 
 def mine():
-  block.propagate()
   wordChain[-1]["body"] = [block.word() for _ in range(config["blockchain"]["maxWords"])]
   merkleRoot = wordChain[-1]["body"]
   while len(merkleRoot) > 1:
